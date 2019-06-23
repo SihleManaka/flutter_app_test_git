f4ac5097008d428bd0fa33823c9a637ac6c9cf4f
SihleManaka <johannesmanaka71@gmail.com>
2019-06-23 16:01:29 +0200

    Initial commit

---
+++ android/app/src/main/java/com/fluttermodule2longlist/flutter_app_test_git/MainActivity.java
@@ -0,0 +1,13 @@
+package com.fluttermodule2longlist.flutter_app_test_git;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
